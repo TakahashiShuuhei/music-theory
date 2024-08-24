fd6457e00791ed8f6934cbbdb727fa043e698e9f
uhiaha888 <uhiaha888@gmail.com>
2024-08-24 23:03:07 +0900

    一部クラスのドキュメントが生成されない問題を修正

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { PitchClass } from './PitchClass';
 export { Pitch } from './Pitch';
 export { Note } from './Note';
+export { Interval } from './Interval';
+export { Chord, ChordQuality } from './Chord';
+export { Key, MajorKey, MinorKey } from './Key';
 
 // 必要に応じて、追加の型やユーティリティ関数をここでエクスポートできます
